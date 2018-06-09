9be83d2ea0c4a5ba83f310076f6cbee2927d3611
wangyiping <61780374@163.com>
2018-06-09 12:04:27 +0800

    feature:添加登录判断,未登录跳转登录页

--- src/router/index.js
+++ src/router/index.js
@@ -18,37 +18,55 @@ export default new Router({
 			component: resolve => require(['../components/register'], resolve)
 		},
 		{
-			path:'/user',
-			component:home,
-			children:[
-				{path:'',component: resolve => require(['../components/user/index'], resolve)},
-				{path:'detail',component: resolve => require(['../components/user/detail'], resolve)},
-				{path:'edit/:id',component: resolve => require(['../components/user/edit'], resolve)},
-				{path:'users',component:resolve => require(['../components/user/list'], resolve)},
-				{path:'changeR/:id',component:resolve => require(['../components/user/changeR'], resolve)},
-				{path:'changeD/:id',component:resolve => require(['../components/user/changeD'], resolve)}
-			]
+			path: '/user',
+			component: home,
+			children: [
+				{ path: '', component: resolve => require(['../components/user/index'], resolve) },
+				{ path: 'detail', component: resolve => require(['../components/user/detail'], resolve) },
+				{ path: 'edit/:id', component: resolve => require(['../components/user/edit'], resolve) },
+				{ path: 'users', component: resolve => require(['../components/user/list'], resolve) },
+				{ path: 'changeR/:id', component: resolve => require(['../components/user/changeR'], resolve) },
+				{ path: 'changeD/:id', component: resolve => require(['../components/user/changeD'], resolve) }
+			],
+			beforeEnter: (to, from, next) => {
+				if(sessionStorage.getItem("user")){
+					next()
+				}else{
+					next(from)
+				}
+			}
 		},
 		{
-			path:'/admin',
-			component:home,
-			children:[
-				{path:'',component: resolve => require(['../components/admin/index'], resolve)},
-				{path:'detail/:id',component: resolve => require(['../components/admin/detail'], resolve)},
-				{path:'edit/:id',component: resolve => require(['../components/admin/edit'], resolve)},
-				{name:'users',path:'users',component:resolve => require(['../components/admin/list'], resolve)},
-				{path:'departments',component:resolve => require(['../components/admin/department'], resolve)},
-				{path:'addDepartments',component:resolve => require(['../components/admin/addDepartment'], resolve)},
-				{path:'departmentVerify',component:resolve => require(['../components/admin/departmentVerify.vue'], resolve)},
-				{path:'registerVerify',component:resolve => require(['../components/admin/registerVerify'], resolve)},
-				{path:'roleVerify',component:resolve => require(['../components/admin/roleVerify'], resolve)}
-			]
+			path: '/admin',
+			component: home,
+			children: [
+				{ path: '', component: resolve => require(['../components/admin/index'], resolve) },
+				{ path: 'detail/:id', component: resolve => require(['../components/admin/detail'], resolve) },
+				{ path: 'edit/:id', component: resolve => require(['../components/admin/edit'], resolve) },
+				{ name: 'users', path: 'users', component: resolve => require(['../components/admin/list'], resolve) },
+				{ path: 'departments', component: resolve => require(['../components/admin/department'], resolve) },
+				{ path: 'addDepartments', component: resolve => require(['../components/admin/addDepartment'], resolve) },
+				{ path: 'departmentVerify', component: resolve => require(['../components/admin/departmentVerify.vue'], resolve) },
+				{ path: 'registerVerify', component: resolve => require(['../components/admin/registerVerify'], resolve) },
+				{ path: 'roleVerify', component: resolve => require(['../components/admin/roleVerify'], resolve) }
+			],
+			beforeEnter: (to, from, next) => {
+				if(sessionStorage.getItem("user")){
+					if(JSON.parse(sessionStorage.getItem("user")).isAdmin){
+						next()
+					}else{
+						next(from)
+					}
+				}else{
+					next(from)
+				}
+			}
 		},
 		{
-			path:'/404',
-			component:home,
-			children:[
-				{path:'',component:resolve => require(['../components/common/404'], resolve)}
+			path: '/404',
+			component: home,
+			children: [
+				{ path: '', component: resolve => require(['../components/common/404'], resolve) }
 			]
 		},
 		{
